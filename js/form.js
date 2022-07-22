cf63a168f057b26c0d4bb7ae55e129cb1ac8a9fe
Sofya Omelyanyk <107050400+sofyaomelyanyk@users.noreply.github.com>
2022-07-22 23:14:24 +0300

    Initial commit

---
+++ js/form.js
@@ -0,0 +1,11 @@
+(() => {
+   document
+   .querySelector('.form')
+   .addEventListener('submit' , e =>{
+      e.preventDefault();
+
+      new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`), 
+      );
+   });
+ })();
